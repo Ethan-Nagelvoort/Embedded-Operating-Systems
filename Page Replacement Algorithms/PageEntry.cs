e3f7ee0cd6ef36ae8924eddc5702b8441d5ca42e
Ethan-Nagelvoort <76789597+Ethan-Nagelvoort@users.noreply.github.com>
2021-12-04 13:44:26 -0800

    Add files via upload

---
+++ Page Replacement Algorithms/PageEntry.cs
@@ -0,0 +1,36 @@
+﻿public class PageEntry
+{
+    private int pagetable;
+    private int dirty; // 1 for write, 0 for read
+    private int VPN;
+
+    public void setPagetable(int pagetable)
+    {
+        this.pagetable = pagetable;
+    }
+
+    public int getPageTable()
+    {
+        return this.pagetable;
+    }
+
+    public void setDirtybit(int dirty)
+    {
+        this.dirty = dirty;
+    }
+
+    public int getDirtyBit()
+    {
+        return this.dirty;
+    }
+
+    public void setVPN(int VPN)
+    {
+        this.VPN = VPN;
+    }
+
+    public int getVPN()
+    {
+        return this.VPN;
+    }
+}
---
+++ Page Replacement Algorithms/PageTable.cs
@@ -0,0 +1,43 @@
+﻿public class PageTable
+{
+    //private int index; 
+    //private PageEntry newp = new PageEntry();
+    private PageEntry[] p = new PageEntry[128];
+    /*
+    public void setIndex(int index)
+    {
+        this.index = index;
+    }
+
+    public int getIndex()
+    {
+        return this.index;
+    }
+
+    public void setPageEntry(PageEntry newp)
+    {
+        this.newp = newp;
+    }
+
+    public PageEntry getPageEntry()
+    {
+        return this.newp;
+    }
+    */
+    public void EnterPage(int ind, PageEntry newp)
+    {
+        p[ind] = newp;
+    }
+
+    public int Check(int ind, PageEntry newp)
+    {
+       if(p[ind] == newp)
+        {
+            return 1;
+        }
+       else
+        {
+            return 2;
+        }
+    }
+}
